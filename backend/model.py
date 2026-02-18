404da2ab6c485cd066d73d1c9338f6c76e2ff69a
prince Jha <jhaprince0407@gmail.com>
2026-02-18 09:25:25 +0530

    added dataingestion code

---
+++ backend/model.py
@@ -0,0 +1,3 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+def get_response(query:str)
--- backend/rag/data_ingestion.py
+++ backend/rag/data_ingestion.py
@@ -13,3 +13,26 @@ document=PyPDFLoader(
 )
 
 document=document.load()
+
+
+#splitting the document into chunk
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=800,
+    chunk_overlap=150
+)
+
+chunks = splitter.split_documents(document)
+
+
+#embedding the document(chunks)
+
+embedding_model=HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vector_db=FAISS.from_documents(
+    documents=chunks,
+    embedding=embedding_model
+)
+
+vector_db.save_local(folder_path="../vectorDB",index_name="Faiss_index")
